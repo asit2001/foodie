33af710f87d75da67d5d8ae4b500763c47511125
asit <asitchakudanga@gmail.com>
2025-05-22 12:02:56 +0530

    add ping api

--- backend/Router/index.ts
+++ backend/Router/index.ts
@@ -1,14 +1,17 @@
-import { Router } from "express";
-import RestaurantRouter from "./restaurantRouter";
-import { searchRouter } from "./search";
-import { authRouter } from "./authRouter";
-import { cartRouter } from "./cartRouter";
-import { addressRouter } from "./addressRouter";
+import { Router } from 'express';
+import RestaurantRouter from './restaurantRouter';
+import { searchRouter } from './search';
+import { authRouter } from './authRouter';
+import { cartRouter } from './cartRouter';
+import { addressRouter } from './addressRouter';
 
 const router = Router();
-router.use("/restaurant",RestaurantRouter)
-router.use("/user",authRouter);
-router.use("/user",cartRouter);
-router.use("/user",addressRouter);
+router.get('/ping', (req, res) => {
+    return res.send('pong');
+});
+router.use('/restaurant', RestaurantRouter);
+router.use('/user', authRouter);
+router.use('/user', cartRouter);
+router.use('/user', addressRouter);
 router.use(searchRouter);
-export {router};
+export { router };
